2ea546972a87a8de651acd889a295c3e1338541b
Mathieu Bourgon <matbourgon@gmail.com>
2026-02-17 16:06:23 +0000

    removed _ prefix from glc

--- tools/glc.sh
+++ tools/glc.sh
@@ -2,5 +2,5 @@ if [ $# -eq 0 ];
 then
     grep -c ^ asm/nonmatchings/*/func_*.s | awk '{print $2"\t"$1}' FS=":" | sort -n > linecounts.txt
 else
-    grep -c ^ asm/nonmatchings/_$1/func_*.s | awk '{print $2"\t"$1}' FS=":" | sort -n > linecounts.txt
+    grep -c ^ asm/nonmatchings/$1/func_*.s | awk '{print $2"\t"$1}' FS=":" | sort -n > linecounts.txt
 fi

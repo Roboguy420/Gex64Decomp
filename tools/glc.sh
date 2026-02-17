03e6725b3bdd5fae8bb467819760b69f0872147e
Mathieu Bourgon <matbourgon@gmail.com>
2026-02-17 03:27:36 +0000

    Modified line counting script to accept an optional argument to filter the source folder

--- tools/glc.sh
+++ tools/glc.sh
@@ -1,1 +1,6 @@
-grep -c ^ asm/nonmatchings/*/func_*.s | awk '{print $2"\t"$1}' FS=":" | sort -n > linecounts.txt
+if [ $# -eq 0 ];
+then
+    grep -c ^ asm/nonmatchings/*/func_*.s | awk '{print $2"\t"$1}' FS=":" | sort -n > linecounts.txt
+else
+    grep -c ^ asm/nonmatchings/_$1/func_*.s | awk '{print $2"\t"$1}' FS=":" | sort -n > linecounts.txt
+fi

cf6fdf1753242a3f61e36f3d040afc8030038bf7
Mathieu Bourgon <matbourgon@gmail.com>
2026-02-17 17:22:18 +0000

    spy_code was apparently safe to add

---
+++ src/spy_code.c
@@ -0,0 +1,46 @@
+#include "common.h"
+
+void func_80159720_EADF0(void) {
+}
+
+INCLUDE_ASM("asm/nonmatchings/spy_code", func_80159728_EADF8);
+
+INCLUDE_ASM("asm/nonmatchings/spy_code", func_801597E8_EAEB8);
+
+INCLUDE_ASM("asm/nonmatchings/spy_code", func_80159834_EAF04);
+
+INCLUDE_ASM("asm/nonmatchings/spy_code", func_80159928_EAFF8);
+
+INCLUDE_ASM("asm/nonmatchings/spy_code", func_80159DA4_EB474);
+
+INCLUDE_ASM("asm/nonmatchings/spy_code", func_80159EEC_EB5BC);
+
+INCLUDE_ASM("asm/nonmatchings/spy_code", func_80159F3C_EB60C);
+
+INCLUDE_ASM("asm/nonmatchings/spy_code", func_8015A014_EB6E4);
+
+INCLUDE_ASM("asm/nonmatchings/spy_code", func_8015A098_EB768);
+
+INCLUDE_ASM("asm/nonmatchings/spy_code", func_8015A15C_EB82C);
+
+INCLUDE_ASM("asm/nonmatchings/spy_code", func_8015A278_EB948);
+
+INCLUDE_ASM("asm/nonmatchings/spy_code", func_8015A364_EBA34);
+
+INCLUDE_ASM("asm/nonmatchings/spy_code", func_8015A5E4_EBCB4);
+
+INCLUDE_ASM("asm/nonmatchings/spy_code", func_8015A618_EBCE8);
+
+INCLUDE_ASM("asm/nonmatchings/spy_code", func_8015A704_EBDD4);
+
+INCLUDE_ASM("asm/nonmatchings/spy_code", func_8015A788_EBE58);
+
+INCLUDE_RODATA("asm/nonmatchings/spy_code", D_8015AD70_EC440);
+
+INCLUDE_RODATA("asm/nonmatchings/spy_code", D_8015AE00_EC4D0);
+
+INCLUDE_RODATA("asm/nonmatchings/spy_code", D_8015AE0C_EC4DC);
+
+INCLUDE_RODATA("asm/nonmatchings/spy_code", D_8015AE10_EC4E0);
+
+INCLUDE_ASM("asm/nonmatchings/spy_code", func_8015A844_EBF14);
